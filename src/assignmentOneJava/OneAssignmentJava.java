b868c0c5df43dea4ce14c08652cb7fdfd5aa37b1
3jayanth7 <99715245+3jayanth7@users.noreply.github.com>
2022-02-28 20:23:52 +0530

    WhileLoop_Example
    Using Scanner and System.in with While Loop

--- src/assignmentOneJava/OneAssignmentJava.java
+++ src/assignmentOneJava/OneAssignmentJava.java
@@ -1,10 +1,23 @@
 package assignmentOneJava;
 
+import java.util.Scanner;
+
 public class OneAssignmentJava {
+		public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			Scanner input = new Scanner(System.in);
+			System.out.println("Type a number between 50 and 300: ");
+			int givenInput = input.nextInt();
 
+			while (givenInput < 50 || givenInput > 300) {
+
+				System.out.println("Oops, that number wasn't between 50 and 300. Try again.");
+				givenInput = input.nextInt();
+			}
+
+			System.out.println("The number you typed in was: " + givenInput);
+
+		
 	}
 
 }
